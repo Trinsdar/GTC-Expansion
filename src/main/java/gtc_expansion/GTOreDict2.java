b439fe7ab1a79a3eb7bc2c7e7ef5c3bc6ed8a57a
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-07-09 15:32:00 -0400

    added more oredict stuff

--- src/main/java/gtc_expansion/GTOreDict2.java
+++ src/main/java/gtc_expansion/GTOreDict2.java
@@ -1,12 +1,9 @@
 package gtc_expansion;
 
-import gtc_expansion.material.GTMaterial2;
-import gtclassic.material.GTMaterialGen;
-import net.minecraftforge.oredict.OreDictionary;
+import gtc_expansion.material.GTMaterialDict2;
 
 public class GTOreDict2 {
     public static void init(){
-        OreDictionary.registerOre("gemRedGarnet", GTMaterialGen.getGem(GTMaterial2.GarnetRed, 1));
-        OreDictionary.registerOre("gemYellowGarnet", GTMaterialGen.getGem(GTMaterial2.GarnetYellow, 1));
+        GTMaterialDict2.init();
     }
 }
