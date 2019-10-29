d776ef447237cec3ab0f3e355f087edaa15d23a1
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-10-29 18:44:14 -0400

    fixed molten fluid textures not working sometimes

--- src/main/java/gtc_expansion/proxy/GEClientProxy.java
+++ src/main/java/gtc_expansion/proxy/GEClientProxy.java
@@ -1,7 +1,10 @@
 package gtc_expansion.proxy;
 
 import gtc_expansion.GEIcons;
+import gtc_expansion.GTCExpansion;
 import ic2.core.platform.textures.Ic2Icons;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -18,5 +21,11 @@ public class GEClientProxy extends GECommonProxy {
     @SubscribeEvent
     public void onIconLoad(Ic2Icons.SpriteReloadEvent event) {
         GEIcons.loadSprites();
+    }
+
+    @SubscribeEvent
+    public static void onRegisterTexture(TextureStitchEvent.Pre event) {
+        event.getMap().registerSprite(new ResourceLocation(GTCExpansion.MODID, "fluids/molten"));
+        event.getMap().registerSprite(new ResourceLocation(GTCExpansion.MODID, "fluids/moltenflowing"));;
     }
 }
