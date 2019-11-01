fce0ffeb8760ee61ba295878e666d94f7463b29f
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-10-31 23:18:30 -0400

    added electrolyzer and chemical reactor support

---
+++ src/main/java/gtc_expansion/util/crafttweaker/GEChemicalReactorSupport.java
@@ -0,0 +1,67 @@
+package gtc_expansion.util.crafttweaker;
+
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.IAction;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IIngredient;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import gtc_expansion.tile.GETileChemicalReactor;
+import gtclassic.GTItems;
+import gtclassic.tile.GTTileCentrifuge;
+import gtclassic.util.crafttweaker.GTCraftTweakerActions;
+import ic2.api.recipe.IRecipeInput;
+import ic2.core.item.recipe.entry.RecipeInputItemStack;
+import net.minecraft.item.ItemStack;
+import stanhebben.zenscript.annotations.Optional;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+@ZenClass("mods.gtclassic.ChemicalReactor")
+@ZenRegister
+public class GEChemicalReactorSupport {
+    @ZenMethod
+    public static void addRecipe(IItemStack[] output, IIngredient input1, IIngredient input2, int cells, @Optional(valueLong = 3200L) int totalEu) {
+        GTCraftTweakerActions.apply(new ChemicalReactorRecipeAction(GTCraftTweakerActions.of(input1), GTCraftTweakerActions.of(input2), cells, totalEu, CraftTweakerMC.getItemStacks(output)));
+    }
+
+    @ZenMethod
+    public static void addRecipe(IItemStack[] output, IIngredient input1, IIngredient input2, @Optional(valueLong = 3200L) int totalEu) {
+        addRecipe(output, input1, input2, 0, totalEu);
+    }
+
+    private static final class ChemicalReactorRecipeAction implements IAction {
+
+        private final IRecipeInput input1;
+        private final IRecipeInput input2;
+        private final int totalEu;
+        private final int cells;
+        private final ItemStack[] output;
+
+        ChemicalReactorRecipeAction(IRecipeInput input1, IRecipeInput input2, int cells, int totalEu, ItemStack... output) {
+            this.input1 = input1;
+            this.input2 = input2;
+            this.cells = cells;
+            this.totalEu = totalEu;
+            this.output = output;
+        }
+
+        @Override
+        public void apply() {
+            if (totalEu > 0) {
+                GETileChemicalReactor.addRecipe(input1, input2, cells, totalEu, output);
+            } else {
+                CraftTweakerAPI.logError(CraftTweakerAPI.getScriptFileAndLine() + " > "
+                        + "Eu amount must be greater then 0!!");
+            }
+        }
+
+        @Override
+        public String describe() {
+            return String.format(Locale.ENGLISH, "Add Recipe[%s, %s, %s, %s -> %s] to %s", this.input1, this.input2, this.cells, this.totalEu, Arrays.deepToString(this.output), GTTileCentrifuge.RECIPE_LIST);
+        }
+    }
+}
