811503c0be6eae85970030ac9f4ce0e71a6e74a0
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-11-28 21:54:22 -0500

    fixed easy generator recipe removal and made dark ash output 1 carbon dust in place of slag dust in centrifuge

--- src/main/java/gtc_expansion/recipes/GTCXRecipe.java
+++ src/main/java/gtc_expansion/recipes/GTCXRecipe.java
@@ -278,7 +278,8 @@ public class GTCXRecipe {
         GTRecipeCraftingHandler.removeRecipe("ic2", "shaped_tile.blockwatergenerator_-2059790844");
         GTRecipeCraftingHandler.removeRecipe("ic2", "shaped_tile.blockwindgenerator_1669945012");
         GTRecipeCraftingHandler.removeRecipe("ic2", "shapeless_item.itemdustbronze_-364730307");
-        GTRecipeCraftingHandler.removeRecipe("ic2", "shaped_tile.blockgenerator_183901857");
+        GTRecipeCraftingHandler.removeRecipe("ic2", "shaped_tile.blockgenerator_234578637");
+        GTRecipeCraftingHandler.removeRecipe("ic2", "shaped_tile.blockgenerator_183901657");
         ItemStack battery = Ic2Items.battery;
         String circuit = "circuitBasic";
         int recipeId = IC2.config.getFlag("SteelRecipes") ? -342403874 : -1588477206;
