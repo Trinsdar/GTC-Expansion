8e0e5c7025eede3963de6b71dbae95e0d50ad1a2
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-12-14 18:48:06 -0500

    bumped gtc version being checked for

--- src/main/java/gtc_expansion/GTCExpansion.java
+++ src/main/java/gtc_expansion/GTCExpansion.java
@@ -22,7 +22,7 @@ public class GTCExpansion {
 	public static final String MODID = "gtc_expansion";
 	public static final String MODNAME = "GregTech Classic Expansion";
 	public static final String MODVERSION = "@VERSION@";
-	public static final String DEPENDS = "required-after:ic2;required-after:ic2-classic-spmod;required-after:gtclassic@[1.0.8,);after:twilightforest@[3.9.984,);after:ic2c_extras;after:gravisuit@[1.0.8.1,)";
+	public static final String DEPENDS = "required-after:ic2;required-after:ic2-classic-spmod;required-after:gtclassic@[1.0.9,);after:twilightforest@[3.9.984,);after:ic2c_extras;after:gravisuit@[1.0.8.1,)";
 	@SidedProxy(clientSide = MODID + ".proxy.GTCXClientProxy", serverSide = MODID + ".proxy.GTCXCommonProxy")
 	public static GTCXCommonProxy proxy;
 	@Mod.Instance
