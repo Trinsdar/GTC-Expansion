8dde7c7fe715fb20b7efb56c7ad532dd810eeddc
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-12-23 19:10:59 -0500

    added tooltip to magic dye item

--- src/main/java/gtc_expansion/item/GTCXItemMiscSpriteless.java
+++ src/main/java/gtc_expansion/item/GTCXItemMiscSpriteless.java
@@ -1,12 +1,20 @@
 package gtc_expansion.item;
 
 import gtc_expansion.GTCExpansion;
+import gtc_expansion.GTCXItems;
 import gtclassic.GTMod;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class GTCXItemMiscSpriteless extends Item {
 
@@ -16,6 +24,13 @@ public class GTCXItemMiscSpriteless extends Item {
         setRegistryName(this.name.toLowerCase());
         setUnlocalizedName(GTCExpansion.MODID + "." + this.name.toLowerCase());
         setCreativeTab(GTMod.creativeTabGT);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+        if (this == GTCXItems.magicDye){
+            tooltip.add(I18n.format("tooltip.gtc_expansion.magicDye"));
+        }
     }
 
     @SideOnly(Side.CLIENT)
