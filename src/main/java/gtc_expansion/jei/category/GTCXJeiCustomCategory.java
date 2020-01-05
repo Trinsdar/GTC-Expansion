49e7f8701ce43ef2fd2b9f873830850c951ec2af
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-01-04 21:38:22 -0500

    fixed  jei height

--- src/main/java/gtc_expansion/jei/category/GTCXJeiCustomCategory.java
+++ src/main/java/gtc_expansion/jei/category/GTCXJeiCustomCategory.java
@@ -8,4 +8,9 @@ public class GTCXJeiCustomCategory extends GTJeiMultiRecipeCategory {
     public GTCXJeiCustomCategory(IGuiHelper helper, String name, Block block) {
         super(helper, name, block);
     }
+
+    @Override
+    protected int getHeight() {
+        return super.getHeight() + 10;
+    }
 }
