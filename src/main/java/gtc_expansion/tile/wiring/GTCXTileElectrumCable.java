d2eab7837129845d52d4c36097f916b2dd802b8c
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-01-12 16:56:15 -0500

    more cable work, not even testable yet

--- src/main/java/gtc_expansion/tile/wiring/GTCXTileElectrumCable.java
+++ src/main/java/gtc_expansion/tile/wiring/GTCXTileElectrumCable.java
@@ -1,49 +1,68 @@
 package gtc_expansion.tile.wiring;
 
+import gtc_expansion.GTCXBlocks;
+import gtclassic.api.interfaces.IGTItemContainerTile;
 import gtclassic.api.interfaces.IGTRecolorableStorageTile;
+import gtclassic.api.material.GTMaterial;
+import gtclassic.api.material.GTMaterialGen;
 import gtclassic.common.tile.wiring.GTTileSuperconductorCable;
 import ic2.api.classic.energy.tile.IAnchorConductor;
+import ic2.api.classic.energy.tile.IEnergyConductorColored;
+import ic2.api.classic.energy.tile.IInsulationModifieableConductor;
 import ic2.api.classic.network.adv.NetworkField;
 import ic2.api.classic.network.adv.NetworkField.BitLevel;
+import ic2.api.classic.util.IWorldTickCallback;
 import ic2.api.energy.EnergyNet;
 import ic2.api.energy.event.EnergyTileLoadEvent;
 import ic2.api.energy.event.EnergyTileUnloadEvent;
 import ic2.api.energy.tile.IEnergyAcceptor;
-import ic2.api.energy.tile.IEnergyConductor;
 import ic2.api.energy.tile.IEnergyEmitter;
 import ic2.api.energy.tile.IEnergyTile;
 import ic2.api.energy.tile.IMetaDelegate;
 import ic2.api.network.INetworkTileEntityEventListener;
+import ic2.core.IC2;
 import ic2.core.RotationList;
 import ic2.core.block.base.tile.TileEntityBlock;
 import ic2.core.block.base.util.texture.TextureCopyStorage;
 import ic2.core.block.wiring.tile.TileEntityCable;
+import ic2.core.platform.registry.Ic2Items;
+import ic2.core.util.misc.StackUtil;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
-public class GTCXTileElectrumCable extends TileEntityBlock implements IEnergyConductor, IGTRecolorableStorageTile {
+public class GTCXTileElectrumCable extends TileEntityBlock implements IInsulationModifieableConductor, IGTRecolorableStorageTile, IAnchorConductor, IGTItemContainerTile {
 
     @NetworkField(index = 8)
     public RotationList connection;
-    protected boolean addedToEnergyNet;
-    @NetworkField(index = 9)
-    public int color;
     @NetworkField(
-            index = 10
+            index = 9
     )
-    public byte foamed;
+    public RotationList anchors;
+    protected boolean addedToEnergyNet;
+    @NetworkField(index = 10)
+    public int color;
     @NetworkField(
             index = 11
     )
+    public byte foamed;
+    @NetworkField(
+            index = 12
+    )
     public TextureCopyStorage storage;
     @NetworkField(
-            index = 12,
+            index = 13,
             compression = BitLevel.Bit8
     )
     public int insulation;
@@ -54,6 +73,7 @@ public class GTCXTileElectrumCable extends TileEntityBlock implements IEnergyCon
         this.insulation = 0;
         this.foamed = 0;
         this.connection = RotationList.EMPTY;
+        this.anchors = RotationList.EMPTY;
         this.addNetworkFields("connection", NBT_COLOR, "foamed", "storage", "insulation");
     }
 
@@ -84,13 +104,68 @@ public class GTCXTileElectrumCable extends TileEntityBlock implements IEnergyCon
         } else {
             this.color = 16383998;
         }
+        byte newFoamed = nbt.getByte("Foaming");
+        this.insulation = nbt.getInteger("Insulation");
+        this.connection = RotationList.ofNumber(nbt.getByte("Connection"));
+        this.anchors = RotationList.ofNumber(nbt.getByte("Anchors"));
+        if (newFoamed == 1) {
+            this.changeFoam(newFoamed, true);
+        } else {
+            this.foamed = newFoamed;
+        }
+        this.storage.readFromNBT(nbt.getCompoundTag("Storage"));
+
     }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
         super.writeToNBT(nbt);
         nbt.setInteger(NBT_COLOR, this.color);
+        nbt.setByte("Foaming", this.foamed);
+        nbt.setInteger("Insulation", this.insulation);
+        this.storage.writeToNBT(this.getTag(nbt, "Storage"));
         return nbt;
+    }
+
+    public boolean changeFoam(byte foamed) {
+        return this.changeFoam(foamed, false);
+    }
+
+    private boolean changeFoam(byte newFoam, boolean duringLoad) {
+        if (this.foamed == newFoam) {
+            return false;
+        } else {
+            if (this.isSimulating()) {
+                if ((this.foamed = newFoam) == 1) {
+                    this.storage.setAll(IEnergyConductorColored.WireColor.Silver);
+                    if (!duringLoad) {
+                        this.getNetwork().updateTileEntityField(this, "storage");
+                    }
+
+                    IC2.callbacks.addCallback(this.world, new IWorldTickCallback() {
+                        public ActionResult<Integer> tickCallback(World world) {
+                            if (!isInvalid() && foamed == 1) {
+                                if (world.getLightFromNeighbors(getPos()) * 6 >= world.rand.nextInt(1000)) {
+                                    changeFoam((byte)2);
+                                    return ActionResult.newResult(EnumActionResult.SUCCESS, 0);
+                                } else {
+                                    return ActionResult.newResult(EnumActionResult.PASS, 500);
+                                }
+                            } else {
+                                return ActionResult.newResult(EnumActionResult.FAIL, 0);
+                            }
+                        }
+                    }, 500);
+                } else if (this.foamed == 0) {
+                }
+
+                if (!duringLoad) {
+                    this.getNetwork().updateTileEntityField(this, "foamed");
+                }
+            }
+
+            return true;
+        }
     }
 
     @Override
@@ -134,7 +209,7 @@ public class GTCXTileElectrumCable extends TileEntityBlock implements IEnergyCon
 
     @Override
     public void removeInsulation() {
-
+        tryRemoveInsulation();
     }
 
     @Override
@@ -218,5 +293,98 @@ public class GTCXTileElectrumCable extends TileEntityBlock implements IEnergyCon
     @Override
     public boolean isColored() {
         return this.color != 16383998;
+    }
+
+    @Override
+    public boolean addAnchor(EnumFacing enumFacing) {
+        if (this.anchors.contains(enumFacing)) {
+            return false;
+        } else if (this.isRendering()) {
+            return true;
+        } else {
+            if (this.addedToEnergyNet) {
+                MinecraftForge.EVENT_BUS.post(new EnergyTileUnloadEvent(this));
+            }
+
+            this.addedToEnergyNet = false;
+            this.anchors = this.anchors.add(enumFacing);
+            this.getNetwork().updateTileEntityField(this, "anchors");
+            MinecraftForge.EVENT_BUS.post(new EnergyTileLoadEvent(this));
+            this.addedToEnergyNet = true;
+            return true;
+        }
+    }
+
+    @Override
+    public boolean removeAnchor(EnumFacing enumFacing) {
+        if (this.anchors.notContains(enumFacing)) {
+            return false;
+        } else {
+            if (this.addedToEnergyNet) {
+                MinecraftForge.EVENT_BUS.post(new EnergyTileUnloadEvent(this));
+            }
+
+            this.addedToEnergyNet = false;
+            this.anchors = this.anchors.remove(enumFacing);
+            this.getNetwork().updateTileEntityField(this, "anchors");
+            MinecraftForge.EVENT_BUS.post(new EnergyTileLoadEvent(this));
+            this.addedToEnergyNet = true;
+            return true;
+        }
+    }
+
+    @Override
+    public boolean hasAnchor(EnumFacing enumFacing) {
+        return this.anchors.contains(enumFacing);
+    }
+
+    @Override
+    public boolean tryAddInsulation() {
+        if (this.insulation > 3 || this.insulation < 0){
+            return false;
+        }
+        insulation += 1;
+        return true;
+    }
+
+    @Override
+    public boolean tryRemoveInsulation() {
+        if (this.insulation <= 0){
+            return false;
+        }
+        insulation -= 1;
+        return true;
+    }
+
+    public Block getBlockDrop() {
+        return GTCXBlocks.advancedWorktable;
+    }
+
+    @Override
+    public List<ItemStack> getDrops() {
+        List<ItemStack> list = new ArrayList<>();
+
+        ItemStack block = GTMaterialGen.get(this.getBlockDrop());
+        if (this.isColored() && this.color != GTMaterial.Electrum.getColor().getRGB()) {
+            NBTTagCompound nbt = StackUtil.getOrCreateNbtData(block);
+            nbt.setInteger("color", this.color);
+        }
+        if (this.insulation > 0){
+            NBTTagCompound nbt = StackUtil.getOrCreateNbtData(block);
+            nbt.setInteger("insulation", this.insulation);
+        }
+
+        for(int i = 0; i < 6; ++i) {
+            if (this.anchors.contains(EnumFacing.getFront(i))) {
+                list.add(Ic2Items.miningPipe.copy());
+            }
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<ItemStack> getInventoryDrops() {
+        return null;
     }
 }
