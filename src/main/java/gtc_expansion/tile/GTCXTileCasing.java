f983bb6b18af7bfdb1912dde566f55d94a0be4d4
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-05-10 14:10:15 -0400

    added debug info to scanner

--- src/main/java/gtc_expansion/tile/GTCXTileCasing.java
+++ src/main/java/gtc_expansion/tile/GTCXTileCasing.java
@@ -1,9 +1,14 @@
 package gtc_expansion.tile;
 
+import gtc_expansion.GTCXBlocks;
+import gtc_expansion.block.GTCXBlockCasing;
+import gtclassic.api.interfaces.IGTDebuggableTile;
 import ic2.core.block.base.tile.TileEntityBlock;
 import net.minecraft.nbt.NBTTagCompound;
 
-public class GTCXTileCasing extends TileEntityBlock {
+import java.util.Map;
+
+public class GTCXTileCasing extends TileEntityBlock implements IGTDebuggableTile {
     public int rotor;
     public GTCXTileCasing(){
         super();
@@ -29,5 +34,13 @@ public class GTCXTileCasing extends TileEntityBlock {
     public void readFromNBT(NBTTagCompound nbt) {
         super.readFromNBT(nbt);
         rotor = nbt.getInteger("rotor");
+    }
+
+    @Override
+    public void getData(Map<String, Boolean> map) {
+        map.put("Rotor: " + rotor, false);
+        if (world.getBlockState(pos).getBlock() == GTCXBlocks.casingStandard){
+            map.put("Blockstate Rotor: " + world.getBlockState(pos).getValue(GTCXBlockCasing.rotor), false);
+        }
     }
 }
