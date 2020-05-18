4956551d51ec51c97e8b56e05b410b99869129a3
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-05-17 20:58:15 -0400

    added torch placement to drill

---
+++ src/main/java/gtc_expansion/GTCXPlugin.java
@@ -0,0 +1,31 @@
+package gtc_expansion;
+
+import gtc_expansion.item.overrides.GTCXItemDrill;
+import ic2.api.classic.addon.IC2Plugin;
+import ic2.api.classic.addon.PluginBase;
+import ic2.api.classic.addon.misc.IOverrideObject;
+import ic2.core.platform.lang.components.base.LocaleComp;
+import ic2.core.platform.lang.storage.Ic2ItemLang;
+import ic2.core.util.misc.ModulLoader;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+import java.util.Map;
+
+@IC2Plugin(id = "gtcx_plugin", name = "GTCX Plugin", version = GTCExpansion.MODVERSION)
+public class GTCXPlugin extends PluginBase {
+    @Override
+    public boolean canLoad(Side side) {
+        return true;
+    }
+
+    @Override
+    public void preInit(FMLPreInitializationEvent event, Map<String, IOverrideObject> map)
+    {
+        map.put(getID(Ic2ItemLang.drillItem), new ModulLoader.ItemOverride(new GTCXItemDrill()));
+    }
+
+    private static String getID(LocaleComp comp) {
+        return comp.getUnlocalized().replace("item.", "");
+    }
+}
