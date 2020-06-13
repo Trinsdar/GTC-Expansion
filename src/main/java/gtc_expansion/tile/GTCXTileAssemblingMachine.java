790fd80b679b94dbd5bc8b8b5fc5314171be950e
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-06-13 15:27:56 -0400

    fixed machine block recipe conflict with steel hull

--- src/main/java/gtc_expansion/tile/GTCXTileAssemblingMachine.java
+++ src/main/java/gtc_expansion/tile/GTCXTileAssemblingMachine.java
@@ -159,7 +159,7 @@ public class GTCXTileAssemblingMachine extends GTTileBaseMachine {
         addRecipe(GTCXValues.getRefinedIronPlate(), 2, Ic2Items.electricCircuit, 12800, GTMaterialGen.get(GTCXItems.machineParts, 4));
         addRecipe("platePlatinum", 1, Ic2Items.advancedCircuit, 51200, GTMaterialGen.get(GTCXItems.processorCircuitBoard));
         addRecipe(new RecipeInputCombined(8, input("gemEmerald", 8), input("gemOlivine", 8)), new RecipeInputItemStack(Ic2Items.advancedCircuit), 51200, GTMaterialGen.get(GTItems.chipData, 4));
-        addRecipe(GTCXValues.getRefinedIronPlate(), 6, GTMaterialGen.get(GTCXItems.machineParts), 3200, Ic2Items.machine);
+        addRecipe(GTCXValues.REFINED_IRON, 6, GTMaterialGen.get(GTCXItems.machineParts), 3200, Ic2Items.machine);
         addRecipe("dustFlint", 5, GTMaterialGen.get(Blocks.TNT, 3), 1600, GTMaterialGen.getIc2(Ic2Items.industrialTNT, 5));
         addRecipe("dustGunpowder", 4, GTMaterialGen.get(Blocks.SAND), 400, GTMaterialGen.get(Blocks.TNT));
         addRecipe(input("dustGlowstone", 1), GTCXValues.ANY_LAPIS, 1600, GTMaterialGen.get(GTCXItems.advancedCircuitParts, 2));
