04e6358c595f0150a4557788f593428c8f026f3a
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2020-09-25 21:29:12 -0400

    actually fixed upgrades not working in scanner

--- src/main/java/gtc_expansion/tile/overrides/GTCXTileScanner.java
+++ src/main/java/gtc_expansion/tile/overrides/GTCXTileScanner.java
@@ -8,7 +8,6 @@ import gtc_expansion.container.GTCXContainerScanner;
 import gtc_expansion.data.GTCXLang;
 import ic2.api.classic.item.IMachineUpgradeItem;
 import ic2.api.crops.ICropSeed;
-import ic2.api.energy.EnergyNet;
 import ic2.core.RotationList;
 import ic2.core.block.machine.low.TileEntityCropAnalyzer;
 import ic2.core.inventory.base.IHasInventory;
@@ -153,6 +152,7 @@ public class GTCXTileScanner extends TileEntityCropAnalyzer {
                     this.progress = 0;
                     this.getNetwork().updateTileGuiField(this, "progress");
                 }
+                doUpgradeStuff();
                 return;
             }
             stack = GeneticsUtil.convertToGeneticEquivalent(stack);
@@ -162,6 +162,7 @@ public class GTCXTileScanner extends TileEntityCropAnalyzer {
                     this.progress = 0;
                     this.getNetwork().updateTileGuiField(this, "progress");
                 }
+                doUpgradeStuff();
                 return;
             }
             if (!stack.isEmpty()) {
@@ -171,6 +172,7 @@ public class GTCXTileScanner extends TileEntityCropAnalyzer {
                         this.progress = 0;
                         this.getNetwork().updateTileGuiField(this, "progress");
                     }
+                    doUpgradeStuff();
                     return;
                 }
                 if (this.hasEnergy(this.energyUsage)) {
@@ -184,12 +186,7 @@ public class GTCXTileScanner extends TileEntityCropAnalyzer {
                             NBTTagCompound nbttagcompound = new NBTTagCompound();
                             individual.writeToNBT(nbttagcompound);
                             stack.setTagCompound(nbttagcompound);
-                            for(int i = 0; i < 4; ++i) {
-                                ItemStack upgrade = (ItemStack)this.inventory.get(i + this.inventory.size() - 4);
-                                if (upgrade.getItem() instanceof IMachineUpgradeItem) {
-                                    ((IMachineUpgradeItem)upgrade.getItem()).onProcessFinished(upgrade, this);
-                                }
-                            }
+                            doUpgradeStuff();
                             this.notifyNeighbors();
                         }
                         this.inventory.set(2, stack);
@@ -209,53 +206,16 @@ public class GTCXTileScanner extends TileEntityCropAnalyzer {
             this.progress = 0;
             this.getNetwork().updateTileGuiField(this, "progress");
         }
+        doUpgradeStuff();
+        this.updateComparators();
+    }
+
+    private void doUpgradeStuff(){
         for(int i = 0; i < 4; ++i) {
             ItemStack item = this.inventory.get(i + this.inventory.size() - 4);
             if (item.getItem() instanceof IMachineUpgradeItem) {
                 ((IMachineUpgradeItem)item.getItem()).onTick(item, this);
             }
         }
-
-        this.updateComparators();
-    }
-
-    @Override
-    public void setOverclockerRates() {
-        int extraEnergyDemand = 0;
-        double energyDemandMultiplier = 1.0D;
-        int extraEnergyStorage = 0;
-        double energyStorageMultiplier = 1.0D;
-        int extraTier = 0;
-
-        for(int i = 0; i < 4; ++i) {
-            ItemStack item = (ItemStack)this.inventory.get(i + this.inventory.size() - 4);
-            if (item != null && item.getItem() instanceof IMachineUpgradeItem) {
-                IMachineUpgradeItem upgrade = (IMachineUpgradeItem)item.getItem();
-                upgrade.onInstalling(item, this);
-                extraEnergyDemand += upgrade.getExtraEnergyDemand(item, this) * item.getCount();
-                energyDemandMultiplier *= Math.pow(upgrade.getEnergyDemandMultiplier(item, this), (double)item.getCount());
-                extraEnergyStorage += upgrade.getExtraEnergyStorage(item, this) * item.getCount();
-                energyStorageMultiplier *= Math.pow(upgrade.getEnergyStorageMultiplier(item, this), (double)item.getCount());
-                extraTier += upgrade.getExtraTier(item, this) * item.getCount();
-            }
-        }
-
-        this.energyUsage = applyModifier(1, extraEnergyDemand, energyDemandMultiplier);
-        this.setMaxEnergy(applyModifier(10000, extraEnergyStorage, energyStorageMultiplier));
-        this.tier = 1 + extraTier;
-        if (this.tier > 13) {
-            this.tier = 13;
-        }
-
-        this.maxInput = (int) EnergyNet.instance.getPowerFromTier(this.tier);
-        if (this.energyUsage < 1) {
-            this.energyUsage = 1;
-        }
-
-    }
-
-    static int applyModifier(int base, int extra, double multiplier) {
-        long ret = Math.round((double)(base + extra) * multiplier);
-        return ret > 2147483647L ? 2147483647 : (int)ret;
     }
 }
